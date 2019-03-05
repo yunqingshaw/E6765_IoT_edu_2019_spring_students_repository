05e2ad81702e596769a02e88d1b11471f0ed5167
Peter <peterjwei1993@gmail.com>
2019-03-05 17:50:39 -0500

    Add files via upload

---
+++ MLHelper.py
@@ -0,0 +1,72 @@
+from sklearn import linear_model, svm
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split, cross_val_score
+import csv
+
+class MLModule:
+	def loadData(self, csvFile):
+		data = []
+		with open(filename, 'rb') as csvfile:
+			reader = csv.reader(csvfile, delimiter = ',')
+			for row in reader:
+				data.append(row)
+
+	def regression(self, data, target):
+		model = None
+		##########################################
+		# 1. Set model to linear regression model
+		##########################################
+		# Hint: use linear_model
+
+		##########################################
+		# 2. Split train and test datasets 70/30
+		##########################################
+		# Hint: use train_test_split. 
+
+
+		##########################################
+		# 3. Fit model to training data
+		##########################################
+
+
+		##########################################
+		# 4. Predict on test data
+		##########################################
+
+
+		##########################################
+		# 5. Evaluate with MAE and MSE
+		##########################################
+		MAE = 0
+		MSE = 0
+
+
+
+
+		print("Mean absolute error: " + str(MAE))
+		print("Mean squared error: " + str(MSE))
+		return model
+
+	def classification(self, data, target):
+		model = None
+		##########################################
+		# 1. Set model to svm classifier
+		##########################################
+
+
+		##########################################
+		# 2. Fit model to training data
+		##########################################
+
+
+		##########################################
+		# 3. Cross Validation
+		##########################################
+		# Hint: Use cross_val_score
+		CVS = 0
+
+
+
+
+		print("Cross validation score: " + str(CVS))
+		return model
